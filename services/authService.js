4b4f0f5ed76b4d86ea33610f8a24a986570f4548
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2024-05-22 12:51:26 +0530

    Finalized Login Function Component by reomving older logging functionality

--- services/authService.js
+++ services/authService.js
@@ -3,30 +3,6 @@ const AuthorRepository = require("../repository/authorRepository");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-// const login = async (userType, email, password) => {
-//   let user;
-//   if (userType === "user") {
-//     user = await UserRepository.findUserByEmail(email);
-//   } else if (userType === "author") {
-//     user = await AuthorRepository.findAuthorByEmail(email);
-//   }
-
-//   if (!user || !(await bcrypt.compare(password, user.password))) {
-//     return { error: "Invalid login credentials" };
-//   }
-
-//   const token = jwt.sign({ id: user.id, userType }, process.env.JWT_SECRET);
-
-//   user.lastLoggedIn = new Date();
-//   await user.save();
-
-//   const { updatedAt, createdAt, password, ...userWithoutTimestamps } = user.toJSON();
-
-//   return { token, userType, user: userWithoutTimestamps };
-// };
-
-// module.exports = { login };
-
 const login = async (userType, email, password) => {
     let user;
     if (userType === "user") {
